6fbb69b4a77dcb74f1a7fa96d768b649303ba37c
deeps4 <deepasharma04@yahoo.com>
2024-03-23 16:54:52 +0530

    Remove all existing code and add a pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-alert('Hello World');
-
-var favoriteFood = 'Indian Food';
-document.write(favoriteFood);
+let pokemonList = [
+      {name: 'Charmander',
+       height: 6,
+       type: 'Fire' },
+      {name: 'Wartortle',
+       height: 1,
+       type:'Water'},
+      {name: 'Rattata',
+       height: 3,
+       type:'Normal'},
+      {name:'Pikachu',
+       height: 4,
+       type:'Electric'}
+]
