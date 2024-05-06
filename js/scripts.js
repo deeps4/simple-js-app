621e21cd99d45ddb5b8eacd04ed9ffd4bde60181
deeps4 <deepasharma04@yahoo.com>
2024-05-06 19:26:16 +0200

    Update the changes.
    Wrap pokemonList in IIFE, Create a new pokemonRepository, add forEach loop,

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,43 @@
-let pokemonList = [
-      {name: 'Charmander',
-       height: 6,
-       type: 'Fire' },
-      {name: 'Wartortle',
-       height: 1,
-       type:'Water'},
-      {name: 'Rattata',
-       height: 3,
-       type:'Normal'},
-      {name:'Pikachu',
-       height: 4,
-       type:'Electric'}
-]
-console.log(pokemonList);
 
-for(let i= 0; i < pokemonList.length; i++){
-    const pokemon= pokemonList[i];
-    console.log(` "${pokemon.name} (height:${pokemon.height})" `);
-    if(pokemon.height > 4){
-        console.log("-Wow, that's big!")
+const pokemonRepository = (function(){
+    const pokemonList = [
+        {name: 'Charmander',
+         height: 6,
+         type: 'Fire' },
+        {name: 'Wartortle',
+         height: 1,
+         type:'Water'},
+        {name: 'Rattata',
+         height: 3,
+         type:'Normal'},
+        {name:'Pikachu',
+         height: 4,
+         type:'Electric'}
+  ]
+    return {
+        getAll: function(){
+            return pokemonList
+
+        },
+        add: function(pokemon){
+            pokemonList.push(pokemon)
+
+        }
     }
-} 
+
+})()
+
+pokemonRepository.add({name: 'Balbasaur',
+ height:3,
+ type: 'Electric'
+ })
+
+ pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(  pokemon.name +' height: ' + pokemon.height);
+    if(pokemon.height< 4){
+        document.write('<br>')
+    }
+    if(pokemon.height > 4){
+        document.write(" -Wow ,that's big! <br> ");
+    }
+});
